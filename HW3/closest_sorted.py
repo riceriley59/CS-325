6bbd8c4ed4513d18019933a7ba4fbb6ec82e48e6
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-04-18 19:56:52 -0700

    finished HW3

--- HW3/closest_sorted.py
+++ HW3/closest_sorted.py
@@ -1,20 +1,20 @@
 from bisect import bisect_left
 
-def find(A, x, k):
-    i = bisect_left(A, x)
+def find(arr: list, x: float, k: int) -> list:
+    i = bisect_left(arr, x)
     
     left = (i - 1)
     right = i
     
-    while k > 0 and (left >= 0 or right <= len(A)):
+    while k > 0 and (left >= 0 or right <= len(arr)):
         if left < 0:
             right += 1
-        elif right == len(A):
+        elif right == len(arr):
             left -= 1
-        elif abs(A[left] - x) <= abs(A[right] - x):
+        elif abs(arr[left] - x) <= abs(arr[right] - x):
             left -= 1
         else:
             right += 1
         k -= 1
     
-    return A[left+1:right]
+    return arr[left+1:right]
