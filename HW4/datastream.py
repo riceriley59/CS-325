a2fe97667c3d2ce705df240bbd753c14d9b3c27a
Riley Rice <riceriley59@gmail.com>
2023-04-27 12:35:47 -0700

    added hw4 with test cases

---
+++ HW4/datastream.py
@@ -0,0 +1,7 @@
+def ksmallest(k: int, arr:list) -> list:
+
+    return []
+
+if __name__ == '__main__':
+    print(ksmallest(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
+    print(ksmallest(3, range(1000000, 0, -1)))
---
+++ HW4/nbest.py
@@ -0,0 +1,16 @@
+from heapq import heapify, heappop, heappush, heapreplace
+
+def nbesta(a: list, b: list) -> list:
+    return []
+
+def nbestb(a: list, b: list) -> list:
+    return []
+
+def nbestc(a: list, b: list) -> list:
+    return []
+
+if __name__ == '__main__':
+    a, b = [4, 1, 5, 3], [2, 6, 3, 4]
+    print(nbesta(a, b))
+    print(nbestb(a, b))
+    print(nbestc(a, b))
