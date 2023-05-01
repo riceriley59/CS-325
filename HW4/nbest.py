d4319032e244da3b0def5bb6e37d8adae915b91c
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-05-01 12:49:15 -0700

    finished hw4

--- HW4/nbest.py
+++ HW4/nbest.py
@@ -1,13 +1,45 @@
-from heapq import heapify, heappop, heappush, heapreplace
+import heapq
+from random import randint
 
 def nbesta(a: list, b: list) -> list:
-    return []
+    nbest = [(x, y) for x in a for y in b]
+    nbest.sort(key=lambda p: (p[0] + p[1], p[1]))
+    return nbest[:len(a)]
+
+def qselect(k: int, a: list) -> int:
+    index = randint(0, len(a)-1) # randomized pivot position
+    a[0], a[index] = a[index] , a[0] # swap pivot with a[0]
+    pivot = a[0]
+        
+    left = [x for x in a if x < pivot]
+    right = [x for x in a[1:] if x >= pivot]
+
+    left_size = len(left) + 1 # including pivot
+    if k == left_size: return pivot
+    return qselect(k, left) if k < left_size else qselect(k - left_size, right)
 
 def nbestb(a: list, b: list) -> list:
-    return []
+    nbest = [(x, y) for x in a for y in b]
+    for i in range(len(a) - 1): nbest.insert(0, qselect(len(a) - i, nbest))
+    return nbest[:len(a)]
 
-def nbestc(a: list, b: list) -> list:
-    return []
+def nbestc(A, B):
+    n = len(A)
+    res = []
+    heap = [(A[0] + B[j], 0, j) for j in range(n)]
+    heapq.heapify(heap)
+    visited = set((0, j) for j in range(n))
+    while len(res) < n:
+        s, i, j = heapq.heappop(heap)
+        res.append((A[i], B[j]))
+        if i+1 < n and (i+1, j) not in visited:
+            heapq.heappush(heap, (A[i+1] + B[j], i+1, j))
+            visited.add((i+1, j))
+        if j+1 < n and (i, j+1) not in visited:
+            heapq.heappush(heap, (A[i] + B[j+1], i, j+1))
+            visited.add((i, j+1))
+    return res
+
 
 if __name__ == '__main__':
     a, b = [4, 1, 5, 3], [2, 6, 3, 4]
--- HW4/team.py
+++ HW4/team.py
@@ -1,7 +1,17 @@
 from heapq import heapify, heappop, heappush, heapreplace
 
 def select(*a: list) -> list:
-    return []
+    output = []
+
+    heap = [(x[0], i, 0) for i, x in enumerate(a)]
+    heapify(heap)
+
+    while heap:
+        x, i, j = heappop(heap)
+        output.append(x)
+        if j < len(a[i]) - 1: heappush(heap, (a[i][j + 1], i, j + 1))
+
+    return output[:len(a[0])]
 
 if __name__ == '__main__':
     print(select([1, 5, 7, 9], [2, 4, 8, 10], [0, 3, 6, 9]))
