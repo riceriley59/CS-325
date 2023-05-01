0166834760e4e30cd89cc0c9001beea1a6e3b68e
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-04-30 19:35:28 -0700

    finished hw4

--- HW4/datastream.py
+++ HW4/datastream.py
@@ -1,6 +1,15 @@
-def ksmallest(k: int, arr:list) -> list:
+import heapq
 
-    return []
+def ksmallest(k, stream):
+    heap = []
+    for i, x in enumerate(stream):
+        if i < k:
+            heapq.heappush(heap, -x)
+        else:
+            if -x > heap[0]:
+                heapq.heapreplace(heap, -x)
+    return sorted([-x for x in heap])
+    
 
 if __name__ == '__main__':
     print(ksmallest(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
---
+++ HW4/team.py
@@ -0,0 +1,8 @@
+from heapq import heapify, heappop, heappush, heapreplace
+
+def select(*a: list) -> list:
+    return []
+
+if __name__ == '__main__':
+    print(select([1, 5, 7, 9], [2, 4, 8, 10], [0, 3, 6, 9]))
+    print(select([16, 18], [0, 10], [5, 7], [2, 9], [11, 19], [8, 17], [6, 13], [1, 11], [14, 16], [1, 4]))
