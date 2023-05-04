1cdb5fc1118e7b1bd45957e974ba84152bde0020
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-05-04 14:20:10 -0700

    added test cases for bottom up solutions

--- HW5/bsts.py
+++ HW5/bsts.py
@@ -1,7 +1,18 @@
+#top-down solution with memoization
 def bsts(n: int) -> int:
     return 0
 
+#bottom-up solution
+def bsts2(n: int) -> int:
+    return 0
+
 if __name__ == '__main__':
+    print("Top-down Solution with memoization:")
     print(bsts(2))
     print(bsts(3))
     print(bsts(5))
+
+    print("Bottom-up solution")
+    print(bsts2(2))
+    print(bsts2(3))
+    print(bsts2(5))
--- HW5/mis.py
+++ HW5/mis.py
@@ -1,15 +1,26 @@
-def max_wis(set: list) -> tuple:
+#top-down with memoization
+def max_wis(graph: list) -> tuple:
     mWeight, mSet = 0, []
+
+    
 
     return (mWeight, mSet)
 
-def max_wis2(set: list) -> tuple:
+#bottom-up solution
+def max_wis2(graph: list) -> tuple:
     mWeight, mSet = 0, []
 
     return (mWeight, mSet)
 
 if __name__ == '__main__':
+    print("Top-down solution with memoization:")
     print(max_wis([7,8,5]))
     print(max_wis([-1,8,10]))
     print(max_wis([]))
     print(max_wis([-5, -1, -4]))
+    
+    print("\nBottom-up solution")
+    print(max_wis2([7,8,5]))
+    print(max_wis2([-1,8,10]))
+    print(max_wis2([]))
+    print(max_wis2([-5, -1, -4]))
