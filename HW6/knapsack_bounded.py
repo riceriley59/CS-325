da9d7037ca30e89657056d4bf9c12d8eff73a601
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-05-15 17:31:20 -0700

    finished hw6

--- HW6/knapsack_bounded.py
+++ HW6/knapsack_bounded.py
@@ -1,5 +1,40 @@
-def best(W: int, triples: list) -> tuple:
-    return (0, 0)
+def backpoint(back: dict, W: int, items: int) -> list:
+    result = [0] * len(items)
+
+    w, i = W, len(items) - 1
+
+    while i >= 0 and w > 0:
+        j = back[(w, i)]
+        result[i] = j
+        w -= j * items[i][0]
+        i -= 1
+
+    return result
+
+def best(W: int, items: list) -> tuple:
+    bestdict, back = {}, {}
+
+    def dp(w: int, i: int) -> int:
+        if (w, i) in bestdict: return bestdict[(w, i)]
+
+        if i < 0 or w <= 0: return 0
+
+        weight, value, count = items[i]
+
+        bestdict[(w, i)] = 0
+
+        for j in range(count + 1):
+            if j * weight > w: break
+
+            val = dp(w - j * weight, i - 1) + j * value
+
+            if val > bestdict[(w, i)]:
+                bestdict[(w, i)] = val
+                back[(w, i)] = j
+
+        return bestdict[(w, i)]
+
+    return (dp(W, len(items) - 1), backpoint(back, W, items))
 
 if __name__ == '__main__':
     print(best(3, [(2, 4, 2), (3, 5, 3)]))
--- HW6/knapsack_unbounded.py
+++ HW6/knapsack_unbounded.py
@@ -1,10 +1,25 @@
-def best(W: int, pairs: list) -> tuple:
-    best = {0: (0, 0)}
+def best(W: int, items: list) -> tuple:
+    best_values = [0] * (W + 1)
+    last_item = [-1] * (W + 1)
+    
+    for weight in range(1, W + 1):
+        best_value = 0
+        for i in range(len(items)):
+            if items[i][0] <= weight:
+                value = best_values[weight - items[i][0]] + items[i][1]
+                if value > best_value:
+                    best_value = value
+                    last_item[weight] = i
 
-    def fit(w: int, pairs: list) -> int:
-        return 0
+        best_values[weight] = best_value
 
-    return (fit(W, pairs), 0)
+    picks = [0] * len(items)
+    weight = W
+    while weight > 0 and last_item[weight] != -1:
+        picks[last_item[weight]] += 1
+        weight -= items[last_item[weight]][0]
+
+    return (best_values[W], picks)
 
 if __name__ == '__main__':
     print(best(3, [(2, 4), (3, 5)]))
