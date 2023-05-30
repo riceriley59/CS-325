225a43512d5b14052e74795b7f77a6e725b6a3f1
Riley Rice <riceriley59@gmail.com>
2023-05-30 09:51:54 -0700

    added test cases for HW9

---
+++ HW9/dijkstra.py
@@ -0,0 +1,7 @@
+def shortest(n: int, edges: list) -> tuple:
+    pass
+
+if __name__ == '__main__':
+    print(shortest(4, [(0,1,1), (0,2,5), (1,2,1), (2,3,2), (1,3,6)]))
+    print(shortest(5, [(0,1,1), (0,2,5), (1,2,1), (2,3,2), (1,3,6)]))
+    print(shortest(4, [(0,1,1), (2,3,1)]))
