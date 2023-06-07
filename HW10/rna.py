3dfea4a1600407987677ce8fbde578cc6f363567
Riley Rice <riceriley59@gmail.com>
2023-06-06 19:45:27 -0700

    added

--- HW10/rna.py
+++ HW10/rna.py
@@ -9,51 +9,84 @@ pairs = {
 
 def best(rna: str) -> tuple:
     n = len(rna)
-    max_pairs = {}
-    back = {}
+    best, back = defaultdict(int), defaultdict(int)
 
-    def _best(start: int, end: int) -> tuple:
-        if start >= end:
-            return 0, ''
+    def _best(start: int, end: int, best: dict, back: dict) -> tuple:
+        if start >= end: return 0, ''
 
-        if (start, end) in max_pairs:
-            return max_pairs[(start, end)], back[(start, end)]
+        if best[(start, end)] != 0: return best[(start, end)], back[(start, end)]
 
-        max_pairs[start, end] = 0
-        max_structure = ''
+        max_pairs = 0; max_structure = ''
 
         if rna[start] in pairs[rna[end]]:
-            pairs_count, structure = _best(start + 1, end - 1)
-            max_pairs[start, end] = pairs_count + 1
+            pairs_count, structure = _best(start + 1, end - 1, best, back)
+            max_pairs = pairs_count + 1
             max_structure = f'({structure})'
 
         for k in range(start, end):
-            left_pairs, left_structure = _best(start, k)
-            right_pairs, right_structure = _best(k + 1, end)
+            left_pairs, left_structure = _best(start, k, best, back)
+            right_pairs, right_structure = _best(k + 1, end, best, back)
 
-            if left_pairs + right_pairs > max_pairs[start, end]:
-                max_pairs[start, end] = left_pairs + right_pairs
+            if left_pairs + right_pairs > max_pairs:
+                max_pairs = left_pairs + right_pairs
                 max_structure = left_structure + right_structure
 
-        back[start, end] = max_structure
+        best[(start, end)] = max_pairs
+        back[(start, end)] = max_structure
 
-        return max_pairs[start, end], max_structure
+        return max_pairs, max_structure
 
-    max_pairs_count, max_structure = _best(0, n - 1)
-    return max_pairs_count, max_structure
+    max_pairs, max_structure = _best(0, n - 1, best, back)
+    return max_pairs, max_structure
 
 def total(rna: str) -> int:
-    total, n = defaultdict(int), len(rna)
+    n = len(rna)
+    count = defaultdict(int)
 
-    return None
+    def _total(start: int, end: int) -> int:
+        if start >= end: return 1
+
+        if count[(start, end)] != 0: return count[(start, end)]
+
+        total_count = _total(start + 1, end)  # Case 1: Ignore current nucleotide
+
+        for k in range(start + 1, end + 1):
+            if rna[start] in pairs[rna[k]]:
+                total_count += _total(start + 1, k - 1) * _total(k + 1, end)
+
+        count[(start, end)] = total_count
+
+        return total_count
+
+    total_count = _total(0, n - 1)
+    return total_count
 
 def kbest(rna: str, k: int) -> list[tuple]:
-    return None
+    results = []
+
+    def _kbest(start: int, end: int, k: int, structure: str):
+        if start >= end or k <= 0:
+            pairs, _ = best(structure)
+            results.append((pairs, structure))
+            return
+
+        if rna[start] in pairs[rna[end]]:
+            _kbest(start + 1, end - 1, k - 1, f'({structure})')
+
+        for i in range(start, end):
+            left_pairs, _ = best(structure[:i - start + 1])
+            right_pairs, _ = best(structure[i - start + 1:])
+
+            if left_pairs + right_pairs >= k:
+                _kbest(start, i, k, structure[:i - start + 1])
+                _kbest(i + 1, end, k, structure[i - start + 1:])
+
+    _kbest(0, len(rna), k, '')
+    results.sort(reverse=True)
+    return results[:k]
 
 if __name__ == '__main__': 
     # Testing the provided examples
-    print(best("ACAGU"))  # (2, '((.))')
-    print(best("GCACG"))  # (2, '().()')
     print(best("UUCAGGA"))  # (3, '(((.)))')
     print(best("GUUAGAGUCU"))  # (4, '(.()((.)))')
     print(best("AUAACCUUAUAGGGCUCUG"))  # (8, '.(((..)()()((()))))')
