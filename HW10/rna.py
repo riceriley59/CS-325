67c5bf04b71bad5b0290d6af220b545e4698fa0c
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2023-06-07 12:06:53 -0700

    testing

--- HW10/rna.py
+++ HW10/rna.py
@@ -1,45 +1,63 @@
 from collections import defaultdict
 
-pairs = {
-    'A': {'U'},
-    'G': {'G'},
-    'C': {'G'},
-    'U': {'A', 'G'}
-}
-
 def best(rna: str) -> tuple:
+    pairs = {'A': 'U', 'U': 'AG', 'C': 'G', 'G': 'CU'}
     n = len(rna)
-    best, back = defaultdict(int), defaultdict(int)
+    best, back = defaultdict(int), defaultdict(lambda: -1)
 
     def _best(start: int, end: int, best: dict, back: dict) -> tuple:
-        if start >= end: return 0, ''
+        if start >= end:
+            return 0, -1
 
-        if best[(start, end)] != 0: return best[(start, end)], back[(start, end)]
+        if best[(start, end)] != 0:
+            return best[(start, end)], back[(start, end)]
 
-        max_pairs = 0; max_structure = ''
+        max_pairs = 0
+        max_k = -1
 
         if rna[start] in pairs[rna[end]]:
-            pairs_count, structure = _best(start + 1, end - 1, best, back)
-            max_pairs = pairs_count + 1
-            max_structure = '({struct})'.format(struct = structure)
+            pairs_count, k = _best(start + 1, end - 1, best, back)
+            if pairs_count + 1 > max_pairs:
+                max_pairs = pairs_count + 1
+                max_k = -1
 
         for k in range(start, end):
-            left, left_str = _best(start, k, best, back)
-            right, right_str = _best(k + 1, end, best, back)
+            left, left_k = _best(start, k, best, back)
+            right, right_k = _best(k + 1, end, best, back)
 
             if left + right > max_pairs:
                 max_pairs = left + right
-                max_structure = left_str + right_str
+                max_k = k
 
         best[(start, end)] = max_pairs
-        back[(start, end)] = max_structure
+        back[(start, end)] = max_k
 
-        return max_pairs, max_structure
+        return max_pairs, max_k
 
-    max_pairs, max_structure = _best(0, n - 1, best, back)
-    return max_pairs, max_structure
+    max_pairs, max_k = _best(0, n - 1, best, back)
+
+    # Generate the final structure string using backpointers
+    structure_string = ['.'] * n
+
+    def fill_structure(start: int, end: int):
+        if start >= end or best[(start, end)] == 0: return
+
+        k = back[(start, end)]
+        if k == -1:
+            structure_string[start] = '('
+            structure_string[end] = ')'
+            fill_structure(start + 1, end - 1)
+        else:
+            fill_structure(start, k)
+            fill_structure(k + 1, end)
+
+    fill_structure(0, n - 1)
+    structure_string = ''.join(structure_string)
+
+    return max_pairs, structure_string
 
 def total(rna: str) -> int:
+    pairs = {'A': 'U', 'U': 'AG', 'C': 'G', 'G': 'CU'}
     n = len(rna)
     count = defaultdict(int)
 
@@ -62,12 +80,22 @@ def total(rna: str) -> int:
     return total_count
 
 if __name__ == '__main__': 
-    # Testing the provided examples
-    print(best("UUCAGGA"))  # (3, '(((.)))')
-    print(best("GUUAGAGUCU"))  # (4, '(.()((.)))')
-    print(best("AUAACCUUAUAGGGCUCUG"))  # (8, '.(((..)()()((()))))')
-    print(best("AACCGCUGUGUCAAGCCCAUCCUGCCUUGUU"))  # (11, '(((.(..(.((.)((...().))()))))))')
-    print(best("GAUGCCGUGUAGUCCAAAGACUUCACCGUUGG"))  # (14, '.()()(()(()())(((.((.)(.))()))))')
-    print(best("CAUCGGGGUCUGAGAUGGCCAUGAAGGGCACGUACUGUUU"))  # (18, '(()())(((((.)))()(((())(.(.().()()))))))')
-    print(best("ACGGCCAGUAAAGGUCAUAUACGCGGAAUGACAGGUCUAUCUAC"))  # (19, '.()(((.)(..))(((.()()(())))(((.)((())))))())')
-    print(best("AGGCAUCAAACCCUGCAUGGGAGCACCGCCACUGGCGAUUUUGGUA"))  # (20, '.(()())...((((()()))((()(.()(((.)))()())))))()')
+    # Test cases
+    test_cases = [
+        "ACAGU",
+        "AC",
+        "GUAC",
+        "GCACG",
+        "CCGG",
+        "CCCGGG",
+        "UUCAGGA",
+        "AUAACCUA",
+        "UUGGACUUG",
+        "UUUGGCACUA",
+        "GAUGCCGUGUAGUCCAAAGACUUC",
+        "AGGCAUCAAACCCUGCAUGGGAGCG"
+    ]
+
+    for s in test_cases:
+        print(best(s))
+        print(total(s))
