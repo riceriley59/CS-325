cf79b76a00a1d8df69fb6913d8be668e85337f55
Riley Rice <riceriley59@gmail.com>
2023-06-06 19:28:56 -0700

    added

--- HW10/rna.py
+++ HW10/rna.py
@@ -9,38 +9,38 @@ pairs = {
 
 def best(rna: str) -> tuple:
     n = len(rna)
-    best, back = defaultdict(int), defaultdict(int)
+    max_pairs = {}
+    back = {}
 
-    def _best(start: int, end: int, best: dict, back: dict) -> tuple:
+    def _best(start: int, end: int) -> tuple:
         if start >= end:
             return 0, ''
 
-        if best[(start, end)] != 0:
-            return best[(start, end)], back[(start, end)]
+        if (start, end) in max_pairs:
+            return max_pairs[(start, end)], back[(start, end)]
 
-        max_pairs = 0
+        max_pairs[start, end] = 0
         max_structure = ''
 
         if rna[start] in pairs[rna[end]]:
-            pairs_count, structure = _best(start + 1, end - 1, best, back)
-            max_pairs = pairs_count + 1
+            pairs_count, structure = _best(start + 1, end - 1)
+            max_pairs[start, end] = pairs_count + 1
             max_structure = f'({structure})'
 
         for k in range(start, end):
-            left_pairs, left_structure = _best(start, k, best, back)
-            right_pairs, right_structure = _best(k + 1, end, best, back)
+            left_pairs, left_structure = _best(start, k)
+            right_pairs, right_structure = _best(k + 1, end)
 
-            if left_pairs + right_pairs > max_pairs:
-                max_pairs = left_pairs + right_pairs
+            if left_pairs + right_pairs > max_pairs[start, end]:
+                max_pairs[start, end] = left_pairs + right_pairs
                 max_structure = left_structure + right_structure
 
-        best[(start, end)] = max_pairs
-        back[(start, end)] = max_structure
+        back[start, end] = max_structure
 
-        return max_pairs, max_structure
+        return max_pairs[start, end], max_structure
 
-    max_pairs, max_structure = _best(0, n - 1, best, back)
-    return max_pairs, max_structure
+    max_pairs_count, max_structure = _best(0, n - 1)
+    return max_pairs_count, max_structure
 
 def total(rna: str) -> int:
     total, n = defaultdict(int), len(rna)
